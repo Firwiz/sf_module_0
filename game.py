9d910d2783f7176731d2b835daa9d54427c42f95
Firwiz <firwiz7@gmail.com>
2020-02-18 21:44:35 +1100

    test

---
+++ game.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def game_core_v3(number):
+
+    count = 0
+    # number - загаданное число
+    # predict - НЕ загаданное число, а то, которое называет кто-то в попытке отгдать загаданное число
+    predict = np.random.randint(1, 100) # рандомный ответот 1 до 100
+
+    newlist = []
+    for i in range(1, 101):
+        newlist.append(i)
+    low = 0
+    high = 100
+
+    while number != predict:
+        count += 1    
+        mid = (low + high) // 2
+        if number > predict:
+            low = mid
+        elif number < predict:
+            high = mid
+        else:
+            break
+        return count
+        print(count)
+
+game_core_v3(5)
+
+
+
+aaaaaaaaaa
