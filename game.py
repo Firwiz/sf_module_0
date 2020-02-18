4c2d037454804f5d7fc6541273af746367afc333
Firwiz <firwiz7@gmail.com>
2020-02-18 23:39:59 +1100

    changed code

--- game.py
+++ game.py
@@ -1,28 +1,22 @@
 import numpy as np
 
-def game_core_v3(number):
-
+def game_core_v4():
     count = 0
-    # number - загаданное число
-    # predict - НЕ загаданное число, а то, которое называет кто-то в попытке отгдать загаданное число
-    predict = np.random.randint(1, 100) # рандомный ответот 1 до 100
-
+    number = np.random.randint(1, 100)
     newlist = []
-    for i in range(1, 101):
+    for i in range(1,101):
         newlist.append(i)
-    low = 0
-    high = 100
-
-    while number != predict:
-        count += 1    
-        mid = (low + high) // 2
-        if number > predict:
+        
+    low = 1 #newlist[0]
+    high = 100 #newlist[100]
+    mid = (low + high) // 2
+    while low <= high:
+        count += 1
+        if number < newlist[mid]:
+           high = mid
+        elif number > newlist[mid]:
             low = mid
-        elif number < predict:
-            high = mid
         else:
             break
-        return count
-        print(count)
-
-game_core_v3(5)
+    return count
+    
